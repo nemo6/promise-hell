632df01147122e11619b88648ed55a2d6e2ddd8c
nemo6 <miguel.maudet.contact@gmail.com>
2022-01-27 19:06:09 +0100

    Create sync.js

---
+++ sync.js
@@ -0,0 +1,20 @@
+function walk_folder(dir,table=[],level=0) {
+
+	let list = fs.readdirSync(dir)
+
+	for ( file of list ){
+
+		let pathx = dir + '\\' + file
+		let stats = fs.statSync(pathx)
+
+		if( stats.isFile() ){
+      table.push(pathx)
+		}
+		else if ( stats.isDirectory() ){
+			level++
+			walk_csv(pathx,table,level)
+		}
+	
+	}
+	return table
+}
