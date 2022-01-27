52c5c6ec5037daec0d5ed8cf58b9ea5e5a197c1b
nemo6 <miguel.maudet.contact@gmail.com>
2022-01-27 19:01:53 +0100

    Create callback.js

---
+++ callback.js
@@ -0,0 +1,48 @@
+(async () => {
+
+v = await walk_folder("test2")
+console.log(v)
+
+})()
+
+function walk_folder(dir,table=[],level=0) {
+
+	return new Promise( (resolve,reject) => {
+	level++
+	fs.readdir( dir, (err,list) => {
+
+		list.forEach( file => {
+
+			let pathx = dir + '\\' + file
+
+			n=0;loop=0
+
+			fs.stat( pathx, (err,stats) => {
+
+				if ( stats.isDirectory() ){
+
+					n++
+					loop++
+					
+					resolve(walk_csv(pathx,table,level))
+				}
+
+				if( stats.isFile() ){
+
+					n++
+					table.push(pathx)
+					
+				}
+
+				// console.log(pathx,n,list.length,loop,`[${level}]`)
+				if( n == list.length && loop == 0 )
+				resolve(table)
+				
+			})
+
+		})
+
+	})
+	})
+
+}
