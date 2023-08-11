e1cace7455c8333e2afd135b318155873f48325d
nemo6 <miguel.maudet.contact@gmail.com>
2023-08-11 07:10:13 +0200

    Update vanilla.js

--- vanilla.js
+++ vanilla.js
@@ -1,21 +1,21 @@
 function walk_folder(dir,table=[],level=0) {
 
-  let list = fs.readdirSync(dir)
-
-  for ( file of list ){
-
-    let pathx = dir + '\\' + file
-    let stats = fs.statSync(pathx)
-
-    if( stats.isFile() ){
-      table.push(pathx)
+    let list = fs.readdirSync(dir)
+    
+    for ( let file of list ){
+    
+        let pathx = dir + '\\' + file
+        let stats = fs.statSync(pathx)
+        
+        if( stats.isFile() ){
+            table.push(pathx)
+        }
+        else if ( stats.isDirectory() ){
+            level++
+            walk_csv(pathx,table,level)
+        }
+    
     }
-    else if ( stats.isDirectory() ){
-      level++
-      walk_csv(pathx,table,level)
-    }
 
-  }
-  return table
-  
+    return table
 }
